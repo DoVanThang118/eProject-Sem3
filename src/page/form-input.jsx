8156ed05033a353fa0a4ec3f84439837d414948b
WinHo <dovanthang118@gmail.com>
2023-08-19 18:23:27 +0700

    sửa trang home

--- src/page/form-input.jsx
+++ src/page/form-input.jsx
@@ -29,7 +29,10 @@ const FormInput = () => {
                             <div className="w-100"><h4>Enter address</h4></div>
                             <div className="form-group">
                                 <div className="select-item">
-                                    <SelectCatagory select={'all'} />
+                                    {/*<SelectCatagory select={'all'} />*/}
+                                    <select className="form-select form-select-sm mb-3" id="city">
+                                        <option value="" selected>Chọn tỉnh thành</option>
+                                    </select>
                                     <div className="select-icon">
                                         <i className="icofont-rounded-down"></i>
                                     </div>
@@ -73,6 +76,50 @@ const FormInput = () => {
                 </div>
             </div>
             <Footer />
+
+            {/*<script>*/}
+            {/*    var citis = document.getElementById("city");*/}
+            {/*    var districts = document.getElementById("district");*/}
+            {/*    var wards = document.getElementById("ward");*/}
+            {/*    var Parameter = {*/}
+            {/*    url: "https://raw.githubusercontent.com/kenzouno1/DiaGioiHanhChinhVN/master/data.json",*/}
+            {/*    method: "GET",*/}
+            {/*    responseType: "application/json",*/}
+            {/*};*/}
+            {/*    var promise = axios(Parameter);*/}
+            {/*    promise.then(function (result) {*/}
+            {/*    renderCity(result.data);*/}
+            {/*});*/}
+
+            {/*    function renderCity(data) {*/}
+            {/*    for (const x of data) {*/}
+            {/*    citis.options[citis.options.length] = new Option(x.Name, x.Id);*/}
+            {/*}*/}
+            {/*    citis.onchange = function () {*/}
+            {/*    district.length = 1;*/}
+            {/*    ward.length = 1;*/}
+            {/*    if(this.value != ""){*/}
+            {/*    const result = data.filter(n => n.Id === this.value);*/}
+
+            {/*    for (const k of result[0].Districts) {*/}
+            {/*    district.options[district.options.length] = new Option(k.Name, k.Id);*/}
+            {/*}*/}
+            {/*}*/}
+            {/*};*/}
+            {/*    district.onchange = function () {*/}
+            {/*    ward.length = 1;*/}
+            {/*    const dataCity = data.filter((n) => n.Id === citis.value);*/}
+            {/*    if (this.value != "") {*/}
+            {/*    const dataWards = dataCity[0].Districts.filter(n => n.Id === this.value)[0].Wards;*/}
+
+            {/*    for (const w of dataWards) {*/}
+            {/*    wards.options[wards.options.length] = new Option(w.Name, w.Id);*/}
+            {/*}*/}
+            {/*}*/}
+            {/*};*/}
+            {/*}*/}
+            {/*</script>*/}
+
         </Fragment>
     );
 }
