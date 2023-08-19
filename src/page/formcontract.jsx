c2898ebd2a4866322433c805020a7679eb4ee7c4
WinHo <dovanthang118@gmail.com>
2023-08-19 18:46:56 +0700

    sửa trang form

--- src/page/formcontract.jsx
+++ src/page/formcontract.jsx
@@ -11,6 +11,10 @@ import PostCategory from "../component/sidebar/post-category";
 import Respond from "../component/sidebar/respond";
 import Search from "../component/sidebar/search";
 import Tags from "../component/sidebar/tags";
+import SelectCatagory from "../component/sidebar/selectCatagory";
+import SelectLanguage from "../component/sidebar/select-language";
+import PriceSelect from "../component/sidebar/price-select";
+import SkillSelect from "../component/sidebar/skill-select";
 
 const btnText = "Đăng kí";
 const socialList = [
@@ -64,57 +68,107 @@ const FormContract = () => {
                                     <div id="respond" className="comment-respond mb-lg-0">
                                      <h4 className="title-border">form đăng kí</h4>
                                      <div className="add-comment">
-                                         <form action="#" method="post" id="commentform" className="comment-form">
-                                                <input 
-                                                    type="text"
-                                                    name="name"
-                                                    className="w-40"
-                                                    placeholder="Tên"
-                                                />  
+                                         <form action="#" method="post" id="commentform" className="contact-form">
+                                                {/*<input */}
+                                                {/*    type="text"*/}
+                                                {/*    name="name"*/}
+                                                {/*    className="w-40"*/}
+                                                {/*    placeholder="Tên"*/}
+                                                {/*/>  */}
                                                 
-                                                 <input 
-                                                    type="text"
-                                                    name="name"
-                                                    placeholder="sdt"
-                                                />
-                                            
-                                                <input 
-                                                    type="text"
-                                                    name="subject"
-                                                    className="w-40"
-                                                    placeholder="địa chỉ"
-                                                />
-                                                   <input 
-                                                    type="text"
-                                                    name="subject"
-                                                    className="w-40"
-                                                    placeholder="số căn cước"
-                                                />
-                                                   <input 
-                                                    type="text"
-                                                    name="subject"
-                                                    className="w-40"
-                                                    placeholder="ngày sinh"
-                                                />
-                                                   <input 
-                                                    type="text"
-                                                    name="subject"
-                                                    className="w-40"
-                                                    placeholder="địa chỉ email"
-                                                />
-                                                   <input 
-                                                    type="text"
-                                                    name="subject"
-                                                    className="w-40"
-                                                    placeholder="Write a Subject"
-                                                />
-                                                <textarea 
-                                                    rows="7" 
-                                                    type="text"
-                                                    name="message"
-                                                    placeholder="Your Message"
-                                                ></textarea>
-                                                <button type="submit" className="lab-btn"><span>{btnText}</span></button>
+                                                {/* <input */}
+                                                {/*    type="text"*/}
+                                                {/*    name="name"*/}
+                                                {/*    placeholder="sdt"*/}
+                                                {/*/>*/}
+
+                                                {/*<input */}
+                                                {/*    type="text"*/}
+                                                {/*    name="subject"*/}
+                                                {/*    className="w-40"*/}
+                                                {/*    placeholder="địa chỉ"*/}
+                                                {/*/>*/}
+                                                {/*   <input */}
+                                                {/*    type="text"*/}
+                                                {/*    name="subject"*/}
+                                                {/*    className="w-40"*/}
+                                                {/*    placeholder="số căn cước"*/}
+                                                {/*/>*/}
+                                                {/*   <input */}
+                                                {/*    type="text"*/}
+                                                {/*    name="subject"*/}
+                                                {/*    className="w-40"*/}
+                                                {/*    placeholder="ngày sinh"*/}
+                                                {/*/>*/}
+                                                {/*   <input */}
+                                                {/*    type="text"*/}
+                                                {/*    name="subject"*/}
+                                                {/*    className="w-40"*/}
+                                                {/*    placeholder="địa chỉ email"*/}
+                                                {/*/>*/}
+                                                {/*   <input */}
+                                                {/*    type="text"*/}
+                                                {/*    name="subject"*/}
+                                                {/*    className="w-40"*/}
+                                                {/*    placeholder="Write a Subject"*/}
+                                                {/*/>*/}
+                                                {/*<textarea */}
+                                                {/*    rows="7" */}
+                                                {/*    type="text"*/}
+                                                {/*    name="message"*/}
+                                                {/*    placeholder="Your Message"*/}
+                                                {/*></textarea>*/}
+                                                {/*<button type="submit" className="lab-btn"><span>{btnText}</span></button>*/}
+                                             <div className="w-100"><h4>Personal information</h4></div>
+                                             <div className="form-group">
+                                                 <input type="text" className="form-control" placeholder="Your Name *" required={true}/>
+                                             </div>
+                                             <div className="form-group">
+                                                 <input type="number" className="form-control" placeholder="Mobile Number *" required={true}/>
+                                             </div>
+
+                                             <div className="w-100"><h4>Enter address</h4></div>
+                                             <div className="form-group">
+                                                 <div className="select-item">
+                                                     <SelectCatagory select={'all'} />
+                                                     <div className="select-icon">
+                                                         <i className="icofont-rounded-down"></i>
+                                                     </div>
+                                                 </div>
+                                             </div>
+                                             <div className="form-group">
+                                                 <div className="select-item">
+                                                     <SelectLanguage select={'all'} />
+                                                     <div className="select-icon">
+                                                         <i className="icofont-rounded-down"></i>
+                                                     </div>
+                                                 </div>
+                                             </div>
+                                             <div className="form-group">
+                                                 <div className="select-item">
+                                                     <PriceSelect select={'all'} />
+                                                     <div className="select-icon">
+                                                         <i className="icofont-rounded-down"></i>
+                                                     </div>
+                                                 </div>
+                                             </div>
+                                             <div className="form-group">
+                                                 <div className="select-item">
+                                                     <SkillSelect select={'all'} />
+                                                     <div className="select-icon">
+                                                         <i className="icofont-rounded-down"></i>
+                                                     </div>
+                                                 </div>
+                                             </div>
+                                             <div className="form-group w-100">
+                                                 <input type="text" className="form-control" placeholder="Address *" required={true}/>
+                                             </div>
+                                             <div className="form-group w-100">
+                                                 <input type="text" className="form-control" placeholder="Address 2"/>
+                                             </div>
+                                             <div className="form-group w-100 text-center mt-4">
+                                                 <button type="submit" className="lab-btn"><span>Next</span></button>
+                                             </div>
                                         
                                          </form>
             </div>			
